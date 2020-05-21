4eaeac315c9ff9691e021ecc9cab6da7fe5a78aa
Kevin BAMOUNI <kevin.bamouni@outlook.com>
2020-05-21 12:14:41 +0200

    fonctions :
    - estimation de la partie sum du triangle des cumuls
    - estimation des incréments à partir des cumuls
    - unscaled pearson residuals
    - psp_phi

--- reserving_functions.R
+++ reserving_functions.R
@@ -2,10 +2,9 @@ library(tidyverse)
 library(matrixStats)
 
 
-##################################################
+##################################################################################
 # Fonction de calcul de reserving par Chain ladder
-##################################################
-
+##################################################################################
 chainLadder_reserving <- function(triangle, type){
   
   # Récupération du nombre de lignes et du nombre de colonne du triangle
@@ -37,9 +36,9 @@ chainLadder_reserving <- function(triangle, type){
 }
 
 
-########################################################################
+##################################################################################
 # Fonction de calcul des coeffisicients de developpement de Chain Ladder
-########################################################################
+##################################################################################
 calcul_coef_de_dev_CL <- function(triangle_des_cumuls){
   
   I <- nrow(triangle_des_cumuls)
@@ -53,9 +52,9 @@ calcul_coef_de_dev_CL <- function(triangle_des_cumuls){
 }
 
 
-########################################################################
+##################################################################################
 # Fonction d'estimation de la partie inferieure du triangle
-########################################################################
+##################################################################################
 
 triangle_inf_estimation <- function(triangle_cumule, coef_de_dev){
   
@@ -72,12 +71,60 @@ triangle_inf_estimation <- function(triangle_cumule, coef_de_dev){
 }
 
 
-########################################################################
+##################################################################################
 # Fonction d'estimation des provisions à effectuer par année d'occurrence
-########################################################################
+##################################################################################
 reserving_est_par_an <- function(matrice_cumulative_estimee){
   I <- nrow(matrice_cumulative_estimee)
   J <- ncol(matrice_cumulative_estimee)
   
   return(matrice_cumulative_estimee[,J] - diag(matrice_cumulative_estimee[,J:1]))
+}
+
+
+##################################################################################
+# Fonction d'estimation de la partie superieure du triangle des cumuls
+##################################################################################
+triangle_sup_estimation <- function(triangle_cumule, coef_de_dev){
+  
+  I <- nrow(triangle_cumule)
+  J <- ncol(triangle_cumule)
+  i <- 1
+  
+  for (j in 1:(J-1)) {
+    triangle_cumule[i,1:(J-j)] <- triangle_cumule[i,(J-j+1)] / cumprod(coef_de_dev[(J-j):1])[length(coef_de_dev[(J-j):1]):1] 
+    i = i+1
+  }
+  
+  return(triangle_cumule)
+}
+
+
+##################################################################################
+# Fonction de calcul du triangle des incréments à partir du triangle des cumuls.
+##################################################################################
+triangle_increment_from_cumuls <- function(triangle_cumule){
+  
+  return(cbind(triangle_cumule[,1],rowDiffs(triangle_cumule)))
+}
+
+
+##################################################################################
+# Fonction de calcul des unscaled pearson residuals upr
+##################################################################################
+
+triangle_upr <- function(triangle_inc, triangle_inc_est){
+  
+  return((triangle_inc - triangle_inc_est) / sqrt(triangle_inc_est))
+}
+
+
+##################################################################################
+# Fonction de calcul pearson scale parameter phi psp_phi
+##################################################################################
+psp_phi <-  function(triangle_upr){
+  
+  I <- nrow(triangle_upr)
+  
+  return(sum(triangle_upr^2, na.rm = TRUE)/(0.5*I(I+1)-2*I+1))
 }
--- run_reserving.R
+++ run_reserving.R
@@ -5,7 +5,4 @@ source("reserving_functions.R")
 # Fichier contenant le tringle input .csv
 payments <- read.csv(file = "triangles_input/triangle_theorique.csv", sep = ";", header = FALSE)
 
-# Type du triangle input : "increment" or "cumulative"
-type = "increment" #or "cumulative"
-
 implem_theorique = chainLadder_reserving(triangle = payments, type = "increment" )
