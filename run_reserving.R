35c7921b1ee1119066a0dbe42a77af11501a1414
Kevin BAMOUNI <kevin.bamouni@outlook.com>
2020-05-25 20:19:49 +0200

    Quelques corrections, et formalisation du résultat attendu!!!

--- run_reserving.R
+++ run_reserving.R
@@ -1,10 +1,25 @@
 source("reserving_functions.R")
  
-############################################### Input nécéssaires ##########################################################
- 
-# Fichier contenant le tringle input .csv
+
+# Input
 payments <- read.csv(file = "triangles_input/triangle_theorique.csv", sep = ";", header = FALSE)
 
+# Chain Ladder
 implem_theorique <-  chainLadder_reserving(triangle = payments)
+implem_theorique[[3]]
+implem_theorique[[5]]
+implem_theorique[[6]]
 
-implem_theorique_bootstrap <- bootstrap_cl_reserving(triangle = payments, number_of_simul = 1000)
+# Bootstrap Chain Ladder
+N = 1000
+implem_theorique_bootstrap <- bootstrap_cl_reserving(triangle = payments, number_of_simul = N)
+
+# Reporting : Chain-ladder model ; bootstrap results
+report <- data.frame("annee"=c("year_1", "year_2", "year_3", "year_4", "year_5", "year_6", "year_7", "year_8", "year_9", "year_10"))
+report$chain_ladder <- implem_theorique[[5]]
+report$bootstrap_mean <- rowMeans(implem_theorique_bootstrap[[7]][1:10,1,1:N])
+report$bootstrap_standard_dev <- rowSds(implem_theorique_bootstrap[[7]][1:10,1,1:N]) # bootstrap ecart type
+report$bootstrap_rmse <- sqrt(rowMeans((implem_theorique[[5]] - implem_theorique_bootstrap[[7]][1:10,1,1:N])^2)) #bootstrap erreur quadratique moyenne
+#report$bootstrap_prediction_error_pct <- (abs(report$bootstrap_prediction_error - report$bootstrap_mean)/report$bootstrap_mean)*100
+
+report
